9fa7bd70586d10afce5ce05e2aacf3e4b376e565
Karjeje <193631637+Karjeje@users.noreply.github.com>
2025-10-23 19:49:35 +0200

    Write the tab-switching logic, import functions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import "./styles.css";
+import {loadHomepage} from "./homepage.js";
+import { loadMenu } from "./menu.js";
+import { loadContactpage } from "./contact.js";
 
 console.log("vafankulo")
+
+const homeBtn = document.querySelector(".homebutton");
+const menuBtn = document.querySelector(".menubutton");
+const contactBtn = document.querySelector(".contactbutton");
+const container = document.querySelector("#content")
+
+homeBtn.addEventListener("click", function() {
+    container.innerHTML = "";
+    loadHomepage();
+});
+
+menuBtn.addEventListener("click", function() {
+    container.innerHTML = "";
+    loadMenu();
+});
+
+contactBtn.addEventListener("click", function() {
+    container.innerHTML = "";
+    loadContactpage();
+});
+
